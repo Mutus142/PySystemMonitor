55d2cee388dc0a9b80fec6a413589f01b4768d60
Mutus <mateus08142@gmail.com>
2026-09-21 20:37:47 -0300

    Criação de arquivos novos + dados cpu pronto

--- Monitor/dadoscpu.py
+++ Monitor/dadoscpu.py
@@ -1,9 +1,57 @@
 import psutil
+import time
 
-while True:
 
-    def dados_cpu()
+def dados_cpu():
 
-        cpu_porcent = psutil.cpu_percent()
+    while True:
+
+        cpu_porcent = psutil.cpu_percent(interval=5)
         cpu_nucleos = psutil.cpu_count(logical=False)
         cpu_threads = psutil.cpu_count(logical=True)
+        cpu_freq = psutil.cpu_freq()
+        cpu_times = psutil.cpu_times()
+
+        print(f'''
+========================================
+              DADOS DA CPU
+========================================
+
+Uso da CPU:          {cpu_porcent}%
+Núcleos físicos:     {cpu_nucleos}
+Threads:             {cpu_threads}
+Frequência:          {cpu_freq}
+Tempos da CPU:       {cpu_times}
+
+========================================
+''')
+
+        time.sleep(7)
+
+        print('''
+========================================
+           O QUE DESEJA FAZER?
+========================================
+
+[1] Voltar ao menu principal
+[2] Fazer uma nova análise da CPU
+[3] Sair
+
+========================================
+''')
+
+        escolha = int(input('Escolha uma opção: '))
+
+        if escolha == 1:
+            return
+
+        elif escolha == 2:
+            print('\nRealizando uma nova análise...\n')
+            continue
+
+        elif escolha == 3:
+            print('\nEncerrando PySystemMonitor...')
+            break
+
+        else:
+            print('\n[!] Opção inválida. Tente novamente.\n')
--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 # MENU INICIAL DE ESCOLHA DE CADA PEÇA/VER MONITOR GERAL 
+from Monitor.dadoscpu import dados_cpu
+from Monitor.dadosram import dados_ram
+from Monitor.dadosdisco import dados_disco
+from Monitor.dadosrede import dados_rede
+from Monitor.analise import analise
 
-while True:
 
-    print('''
-    BEM VINDO AO PY SYSTEM MONITOR
-    1 - VER ANALISE COMPLETA
-    2 - CPU
-    3 - RAM
-    4 - DISCO
-    5 - REDE
+def menu_principal():
     
-    ESCOLHA UMA OPÇÃO''')
+    while True:
 
-    escolha = int(input('Qual é sua escolha?: '))
+        print('''
+        BEM VINDO AO PY SYSTEM MONITOR
+        1 - VER ANALISE COMPLETA
+        2 - CPU
+        3 - RAM
+        4 - DISCO
+        5 - REDE
+    
+        ESCOLHA UMA OPÇÃO''')
 
-    if escolha == 1:
-        analise()
+        escolha = int(input('Qual é sua escolha?: '))
 
-    elif escolha == 2:
-        dados_cpu()
+        if escolha == 1:
+            analise()
 
-    elif escolha == 3:
-        dados_ram()
+        elif escolha == 2:
+            dados_cpu()
 
-    elif escolha == 4:
-        dados_disco()
+        elif escolha == 3:
+            dados_ram()
 
-    elif escolha == 5:
-        dados_rede()
+        elif escolha == 4:
+            dados_disco()
 
-    else:
-        print('Operação invalida!')
-        continue
+        elif escolha == 5:
+            dados_rede()
+
+        else:
+            print('Operação invalida!')
+            continue
