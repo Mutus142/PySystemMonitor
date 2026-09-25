ab9fcc44e1005fa2ccdf2f6a05d5fd6bb5e8bed5
Mutus <mateus08142@gmail.com>
2026-09-24 21:56:44 -0300

    Verificação se ja enviou notificação

--- Utils/alertas.py
+++ Utils/alertas.py
@@ -2,6 +2,12 @@ import psutil
 import time
 from winotify import Notification
 
+alertas_enviados = {
+    "CPU": False,
+    "RAM": False,
+    "DISCO": False
+}
+
 def verificacao_limite():
 
     cpu = psutil.cpu_percent(interval=1)
@@ -16,19 +22,37 @@ def verificacao_limite():
     alerta_ativo = False
 
     if cpu >= 95:
-        enviar_notificacao("CPU: ", cpu)
-        print('Cpu em estado grave!')
-        alerta_ativo = True
+        alerta_ativo = True 
+
+        if not alertas_enviados["CPU"]:
+            enviar_notificacao("CPU", cpu)
+            alertas_enviados["CPU"] = True
+            print("CPU em estado grave!")
+
+    else:
+        alertas_enviados["CPU"] = False
 
     if ram_percent >= 90:
-        enviar_notificacao("RAM: ", ram_percent)
-        print('ram em estado grave!')
         alerta_ativo = True
 
+        if not alertas_enviados["RAM"]:
+            enviar_notificacao("RAM", ram_percent)
+            alertas_enviados["RAM"] = True
+            print("RAM em estado grave!")
+
+    else:
+        alertas_enviados["RAM"] = False
+
     if disco_percent >= 95:
-        enviar_notificacao("DISCO: ", disco_percent)
-        print('disco em estado grave!')
         alerta_ativo = True
+
+        if not alertas_enviados["DISCO"]:
+            enviar_notificacao("DISCO", disco_percent)
+            alertas_enviados["DISCO"] = True
+            print("Disco em estado grave!")
+
+    else:
+        alertas_enviados["DISCO"] = False
 
     if not alerta_ativo:
         print('Componentes funcionando!')
@@ -44,3 +68,6 @@ def enviar_notificacao(recurso, porcentagem):
     )
 
     alerta.show()
+
+alertas_enviados["CPU"] = False
+print('Pode receber notificação nova')
