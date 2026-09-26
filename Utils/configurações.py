15f7e81acbb175eef980b4943158511a0ba1d2b6
Mutus <mateus08142@gmail.com>
2026-09-25 21:55:14 -0300

    Configurações de limites

--- Utils/configurações.py
+++ Utils/configurações.py
@@ -22,7 +22,31 @@ def obter_limites():
         if escolha == 1:
             limite_antigo = limites['CPU']
             print('Limite atual da CPU: ', limite_antigo)
-            
+
             limite_novo = int(input('Qual é o limite novo da CPU?'))
 
             limites['CPU'] = limite_novo
+
+        elif escolha == 2:
+            limite_antigoram = limites['RAM']
+            print('O limite atual da RAM: ', limite_antigoram)
+
+            limite_novoram = int(input('Qual é o novo limite da RAM? '))
+
+            limites['RAM'] = limite_novoram
+
+        elif escolha == 3:
+            limite_antigodisco = limites['DISCO']
+            print('Limite antigo do disco: ', limite_antigodisco)
+
+            limite_novodisco = int(input('Qual é o limite novo do disco? '))
+
+            limites['DISCO'] = limite_novodisco
+
+        else:
+            print('Operação cancelada!')
+
+    return limites
+
+configuracoes = obter_limites()
+print(configuracoes)
